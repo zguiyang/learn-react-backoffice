6cf11ae4a380849adfb98f12a8396f09b609a655
zhaoguiyang <zhhaoguiyang@stacs.cn>
2022-11-21 16:23:18 +0800

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import BasicLayout from './layouts/BasicLayout';
+
+function App() {
+  return (
+    <div className="app">
+      <BasicLayout>
+        <h1>记号记号</h1>
+      </BasicLayout>
+    </div>
+  )
+}
+
+export default App
